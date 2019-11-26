0586da4555f968835c899286a38f4658acde23b7
sergiouph <sergio.uriel.ph@gmail.com>
2019-11-25 19:14:03 -0600

    Add compiler foundations

---
+++ src/main/java/bm/compiler/CProgram.java
@@ -0,0 +1,15 @@
+package bm.compiler;
+
+import org.bm.b5.design.B5Program;
+
+public class CProgram {
+
+    public B5Program compile() {
+        var program = new B5Program();
+
+        
+
+        return program;
+    }
+
+}
---
+++ src/main/java/bm/compiler/ProgramLoader.java
@@ -0,0 +1,11 @@
+package bm.compiler;
+
+import bm.linker.target.TProgram;
+
+public class ProgramLoader {
+
+    public static CProgram loadProgram(TProgram program) {
+        return new CProgram();
+    }
+
+}
